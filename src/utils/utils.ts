1beaf121944c64aa7b0ae45335a88dd799f2479d
chacha <867571123@qq.com>
2020-11-13 13:47:00 +0800

    feat: 添加404错误页面

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,3 +2,35 @@
 const reg = /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(?::\d+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/;
 
 export const isUrl = (path: string): boolean => reg.test(path);
+
+/**
+ * setStorage: 设置localStorage数据
+ * @param key
+ * @param target
+ */
+export const setStorage = (key: string, target: any): void => {
+  const value = JSON.stringify(target);
+  localStorage.setItem(key, value);
+};
+
+/**
+ * getStorage: 获取localStorage数据
+ * @param key
+ */
+export const getStorage = <T>(key: string): T => {
+  const value = localStorage.getItem(key);
+
+  try {
+    return JSON.parse(value as string);
+  } catch {
+    return (value as unknown) as T;
+  }
+};
+
+/**
+ * removeStorage: 移除localStorage数据
+ * @param key
+ */
+export const removeStorage = (key: string): void => {
+  localStorage.removeItem(key);
+};
